3857626732dc28bbb90b00d29082bebecfdc88cb
Ihar-Batura <A17331582@gmail.com>
2024-08-18 12:50:57 +0300

    feat: add some code for app

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,15 +1,22 @@
 const guestName = document.querySelector('.name')
+const city = document.querySelector('.city')
 
 function setLocalStorage() {
   localStorage.setItem('name', guestName.value)
+  localStorage.setItem('city', city.value)
 }
 window.addEventListener('beforeunload', setLocalStorage) //перед перезагрузкой или закрытием страницы (событие beforeunload) данные нужно сохранить
 
 function getLocalStorage() {
   const nameFromLS = localStorage.getItem('name')
+  const cityFromLS = localStorage.getItem('city')
 
   if (nameFromLS !== null) {
     guestName.value = nameFromLS
+  }
+
+  if (city.value !== null) {
+    city.value = cityFromLS
   }
 }
 window.addEventListener('load', getLocalStorage) //перед загрузкой страницы (событие load) данные нужно восстановить и отобразить
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,9 @@
 const weatherIcon = document.querySelector('.weather-icon')
 const temperature = document.querySelector('.temperature')
 const weatherDescription = document.querySelector('.weather-description')
+const wind = document.querySelector('.wind')
+const humidity = document.querySelector('.humidity')
+const weatherError = document.querySelector('.weather-error')
 
 async function getWeather(cityValue) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&lang=en&appid=0b758fa196c64f814ddd05a8bedc75f2&units=metric`
@@ -9,8 +12,12 @@ async function getWeather(cityValue) {
 
   weatherIcon.className = 'weather-icon owf'
   weatherIcon.classList.add(`owf-${data.weather[0].id}`)
-  temperature.textContent = `${data.main.temp}°C`
+  temperature.textContent = `${Math.floor(data.main.temp)}°C`
   weatherDescription.textContent = data.weather[0].description
+  wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`
+  humidity.textContent = `Humidity: ${data.main.humidity}%`
+  weatherError.textContent = ''
+  city.innerHTML = cityValue
 }
 
 /* Добавление в html обработчика событий onchange заменяет всю эту конструкцию! */
